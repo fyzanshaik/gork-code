59334145830ea01e5b5ec6eff9d7380830cc8a47
fyzanshaik <fyzan.shaik@gmail.com>
2025-07-22 23:14:25 +0530

    add read file functionality
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

---
+++ functions/get_file_content.py
@@ -0,0 +1,32 @@
+from functions.config import MAX_CHAR
+import os
+
+def get_file_content(working_directory, file_path):
+    out_of_bound_error = f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    file_not_found_error = f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    full_path = os.path.join(working_directory,file_path)
+    full_path_abs = os.path.abspath(full_path)
+    
+    working_directory_abs = os.path.abspath(working_directory)
+    
+    common_path_abs = os.path.commonpath([full_path_abs,working_directory_abs])
+    
+    if common_path_abs!=working_directory_abs:
+        return out_of_bound_error
+    
+    checkValidFile = os.path.isfile(full_path_abs)
+    if not checkValidFile:
+        return file_not_found_error
+    
+    file_content = ""
+    max_file_content_message = f"[...File '{file_path}' truncated at 10000 characters]"
+    
+    with open(full_path_abs,"r") as f:
+        file_content = f.read()
+        if len(file_content) > MAX_CHAR:
+            file_content = file_content[:1001]+" "+max_file_content_message
+    
+    
+    return file_content
+    
--- tests.py
+++ tests.py
@@ -1,9 +1,19 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+# test_1_result = get_files_info("calculator",".")
+# test_2_result = get_files_info("calculator","pkg")
+# test_3_result = get_files_info("calculator","/bin")
+# test_4_result = get_files_info("calculator","../")
 
-test_1_result = get_files_info("calculator",".")
-test_2_result = get_files_info("calculator","pkg")
-test_3_result = get_files_info("calculator","/bin")
-test_4_result = get_files_info("calculator","../")
+# print(test_1_result)
+# print(test_2_result)
+# print(test_3_result)
+# print(test_4_result)
+
+test_1_result = get_file_content("calculator","main.py")
+test_2_result = get_file_content("calculator","pkg/calculator.py")
+test_3_result = get_file_content("calculator","/bin/cat")
+test_4_result = get_file_content("calculator","pkg/does_not_exist.py")
 
 print(test_1_result)
 print(test_2_result)
